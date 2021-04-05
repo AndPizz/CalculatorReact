7bd2e4c3c4dcc1d4fd4a51917168e051f4fb5f5a
andrewpizzuto300 <andrewpizzuto300@gmail.com>
2021-04-05 08:15:39 +0200

    Design of Calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -4,27 +4,51 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="buttons">
-        <button className="buttonProps">1</button>
-        <button className="buttonProps">2</button>
-        <button className="buttonProps">3</button>
-        <button className="buttonProps">+</button>
+      <>
         <br></br>
-        <button className="buttonProps">4</button>
-        <button className="buttonProps">5</button>
-        <button className="buttonProps">6</button>
-        <button className="buttonProps">-</button>
         <br></br>
-        <button className="buttonProps">7</button>
-        <button className="buttonProps">8</button>
-        <button className="buttonProps">9</button>
-        <button className="buttonProps">*</button>
         <br></br>
-        <button className="buttonProps">.</button>
-        <button className="buttonProps">0</button>
-        <button className="buttonProps">%</button>
-        <button className="buttonProps">=</button>
-      </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <div className="screen"></div>
+        <div className="calculator">
+          <div className="buttons">
+            <button
+              className="buttonProps"
+              value="1"
+              onClick={this.checknumber}
+            >
+              1
+            </button>
+            <button className="buttonProps">2</button>
+            <button className="buttonProps">3</button>
+            <button className="buttonProps">+</button>
+            <br></br>
+            <button className="buttonProps">4</button>
+            <button className="buttonProps">5</button>
+            <button className="buttonProps">6</button>
+            <button className="buttonProps">-</button>
+            <br></br>
+            <button className="buttonProps">7</button>
+            <button className="buttonProps">8</button>
+            <button className="buttonProps">9</button>
+            <button className="buttonProps">*</button>
+            <br></br>
+            <button className="buttonProps">.</button>
+            <button className="buttonProps">0</button>
+            <button className="buttonProps">%</button>
+            <button className="buttonProps">=</button>
+          </div>
+        </div>
+      </>
     );
   }
 }
