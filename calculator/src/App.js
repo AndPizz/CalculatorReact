0c2b5fdb3b40c8b9a5ff6078d80894a80bb779f7
andrewpizzuto300 <andrewpizzuto300@gmail.com>
2021-07-05 10:43:30 +0200

    Screen single digit input

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,6 +2,37 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+    };
+  }
+
+  numberButtonClick = (e) => {
+    var x = e.target.value;
+    console.log(x);
+    this.setState({
+      id: x,
+    });
+  };
+
+  buttonClick = (e) => {
+    var x = e.target.value;
+    console.log(x);
+    this.setState({
+      id: x,
+    });
+  };
+
+  buttonClearScreen = (e) => {
+    var x = e.target.value;
+    console.log(x);
+    this.setState({
+      id: "",
+    });
+  };
+
   render() {
     return (
       <>
@@ -18,34 +49,124 @@ class App extends Component {
         <br></br>
         <br></br>
 
-        <div className="screen"></div>
+        <div className="screen"> {this.state.id}</div>
         <div className="calculator">
           <div className="buttons">
             <button
               className="buttonProps"
               value="1"
-              onClick={this.checknumber}
+              onClick={(e) => this.numberButtonClick(e, "value")}
             >
               1
             </button>
-            <button className="buttonProps">2</button>
-            <button className="buttonProps">3</button>
-            <button className="buttonProps">+</button>
+            <button
+              className="buttonProps"
+              value="2"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              2
+            </button>
+            <button
+              className="buttonProps"
+              value="3"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              3
+            </button>
+            <button
+              className="buttonProps"
+              value="+"
+              onClick={(e) => this.buttonClick(e, "value")}
+            >
+              +
+            </button>
             <br></br>
-            <button className="buttonProps">4</button>
-            <button className="buttonProps">5</button>
-            <button className="buttonProps">6</button>
-            <button className="buttonProps">-</button>
+            <button
+              className="buttonProps"
+              value="4"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              4
+            </button>
+            <button
+              className="buttonProps"
+              value="5"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              5
+            </button>
+            <button
+              className="buttonProps"
+              value="6"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              6
+            </button>
+            <button
+              className="buttonProps"
+              value="-"
+              onClick={(e) => this.buttonClick(e, "value")}
+            >
+              -
+            </button>
             <br></br>
-            <button className="buttonProps">7</button>
-            <button className="buttonProps">8</button>
-            <button className="buttonProps">9</button>
-            <button className="buttonProps">*</button>
+            <button
+              className="buttonProps"
+              value="7"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              7
+            </button>
+            <button
+              className="buttonProps"
+              value="8"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              8
+            </button>
+            <button
+              className="buttonProps"
+              value="9"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              9
+            </button>
+            <button
+              className="buttonProps"
+              value="*"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              *
+            </button>
             <br></br>
-            <button className="buttonProps">.</button>
-            <button className="buttonProps">0</button>
-            <button className="buttonProps">%</button>
-            <button className="buttonProps">=</button>
+            <button
+              className="buttonProps"
+              value="."
+              onClick={(e) => this.buttonClick(e, "value")}
+            >
+              .
+            </button>
+            <button
+              className="buttonProps"
+              value="0"
+              onClick={(e) => this.numberButtonClick(e, "value")}
+            >
+              0
+            </button>
+            <button
+              className="buttonProps"
+              value="C"
+              onClick={(e) => this.buttonClearScreen(e, "value")}
+            >
+              C
+            </button>
+            <button
+              className="buttonProps"
+              value="="
+              onClick={(e) => this.buttonClick(e, "value")}
+            >
+              =
+            </button>
           </div>
         </div>
       </>
